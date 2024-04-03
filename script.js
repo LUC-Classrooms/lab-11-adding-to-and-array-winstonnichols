342a674836e2383de9d08a3698894cc65b2f2186
Winston Nichols <157043193+winstonnichols@users.noreply.github.com>
2024-04-03 15:59:13 +0000

    Lab 11 complete

--- script.js
+++ script.js
@@ -7,49 +7,49 @@ Step 2 - add comments explaining each line of code in this sketch.
 var dots = new Array(1);
 
 function setup() {
-  createCanvas(500, 300); 
-  dots[0] = new Dot(width/2, height/2);
+  createCanvas(500, 300); // size of canvas
+  dots[0] = new Dot(width/2, height/2); 
 }
 
 function draw() {
-  background(200);
+  background(200); // color of background
   
-  for(let i = 0; i < dots.length; i++){
-    dots[i].move(); 
+  for(let i = 0; i < dots.length; i++){  // for loop that continues
+    dots[i].move();
     dots[i].display(); 
   }
-  textSize(24);
-  fill(100, 0, 200);
-  text("'dots' array length: " + dots.length, 100, 100);
+  textSize(24); // font size
+  fill(100, 0, 200); // color of font 
+  text("'dots' array length: " + dots.length, 100, 100); // description
 
 }
 
-function mousePressed(){
-  let obj = new Dot(mouseX, mouseY);
-  
+function mousePressed(){ // when mouse is clicked 
+  let obj = new Dot(mouseX, mouseY); // ^ lets var x and y move 
+  dots.push(obj); // arrays
   /* add a line of code that adds "obj" to the "dots" array. Use the .push() method of the dots array object to append the new "obj" to the end of an existing array. Check the Array.push() documentation first to make sure you are using it correctly.
   */
 
 }
 
 
-function Dot(X, Y){
+function Dot(X, Y){  // initalizing the parameters x annd y and rgb color
   
-  this.x = X;
-  this.y = Y;
-  this.w = random (20, 50);
-  this.sx = random(-5, 5);
-  this.sy = random(-5, 5);
-  this.r = random(0, 255);
-  this.g = random(0, 255);
-  this.b = random(0, 255);
+  this.x = X;  // x is operating 
+  this.y = Y;  // y is operating
+  this.w = random (20, 50); // random dot witdth generated when clicked between values
+  this.sx = random(-5, 5); // speed of dot x when generated when clicked between values
+  this.sy = random(-5, 5); // speed of dot y when generated when clicked between values
+  this.r = random(0, 255);  // random color generated when clicked
+  this.g = random(0, 255); // random color generated when clicked
+  this.b = random(0, 255); // random color generated when clikced
   
-  this.display = function(){
+  this.display = function(){  // rendering the dot
     fill(this.r, this.g, this.b);
     ellipse(this.x, this.y, this.w, this.w);
   }
   
-  this.move = function(){
+  this.move = function(){  // makes the dot move given the var, speed, and size 
     this.x += this.sx;
     this.y += this.sy;
     
